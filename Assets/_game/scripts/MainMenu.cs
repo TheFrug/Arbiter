50982cde2af3e711e4dd1f59df73124ba0c4ebe3
Michael Cabrera <Michael.cabrera.0515@gmail.com>
2026-03-21 10:56:38 -0500

    Main Menu Updates
    Added script

---
+++ Assets/_game/scripts/MainMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string firstLevel;
+    [SerializeField] private GameObject CreditsPanel;
+    [SerializeField] private GameObject OptionsPanel;
+
+    public void StartGame()
+    {
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
+
+    public void OpenCredits()
+    {
+        if (CreditsPanel != null) CreditsPanel.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        if (CreditsPanel != null) CreditsPanel.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Working");
+    }
+}
